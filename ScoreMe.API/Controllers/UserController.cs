fcdb0caad1b024a8e5c40d74a1f85b9b620eb878
imdadhasanli <imdadhasanli@gmail.com>
2020-06-25 18:04:46 +0400

    Change UsersController to UserController

--- ScoreMe.API/Controllers/UserController.cs
+++ ScoreMe.API/Controllers/UserController.cs
@@ -11,92 +11,92 @@ using System.Web.Http.Description;
 
 namespace ScoreMe.API.Controllers
 {
+
+    [RoutePrefix("api/user")]
     public class UserController : ApiController
     {
-        [RoutePrefix("api/user")]
-        public class UsersController : ApiController
+        [HttpGet]
+        [Route("GetUsers")]
+        public List<tbl_User> GetUsers()
         {
-            [HttpGet]
-            [Route("GetUsers")]
-            public List<tbl_User> GetUsers()
-            {
-                CRUDOperation operation = new CRUDOperation();
-                var users = operation.GetUsers(); ;
-                return users;
-            }
+            CRUDOperation operation = new CRUDOperation();
+            var users = operation.GetUsers(); ;
+            return users;
+        }
 
-            [HttpGet]
-            [Route("GetUserByID/{id}")]
-            public tbl_User GetUserByID(Int64 id)
-            {
-                CRUDOperation operation = new CRUDOperation();
-                var user = operation.GetUserById(id); ;
-                return user;
-            }
+        [HttpGet]
+        [Route("GetUserByID/{id}")]
+        public tbl_User GetUserByID(Int64 id)
+        {
+            CRUDOperation operation = new CRUDOperation();
+            var user = operation.GetUserById(id); ;
+            return user;
+        }
 
-            [HttpGet]
-            [Route("GetUserByUserName/{username}")]
-            public tbl_User GetUserByUserName(string username)
+        [HttpGet]
+        [Route("GetUserByUserName/{username}")]
+        public tbl_User GetUserByUserName(string username)
+        {
+            CRUDOperation operation = new CRUDOperation();
+            var user = operation.GetUserByUserName(username); ;
+            return user;
+        }
+        [HttpPost]
+        [ResponseType(typeof(tbl_User))]
+        [Route("AddUser")]
+        public async Task<IHttpActionResult> AddUser(tbl_User item)
+        {
+            if (!ModelState.IsValid)
             {
-                CRUDOperation operation = new CRUDOperation();
-                var user = operation.GetUserByUserName(username); ;
-                return user;
+                return BadRequest(ModelState);
             }
-            [HttpPost]
-            [ResponseType(typeof(tbl_User))]
-            [Route("AddUser")]
-            public async Task<IHttpActionResult> AddUser(tbl_User item)
-            {
-                if (!ModelState.IsValid)
-                {
-                    return BadRequest(ModelState);
-                }
-                CRUDOperation operation = new CRUDOperation();
-                tbl_User dbitem = operation.AddUser(item);
+            CRUDOperation operation = new CRUDOperation();
+            tbl_User dbitem = operation.AddUser(item);
 
+            return Ok(dbitem);
+        }
+
+        [HttpPost]
+        [ResponseType(typeof(tbl_User))]
+        [Route("UpdateUser")]
+        public async Task<IHttpActionResult> UpdateUser(tbl_User item)
+        {
+            CRUDOperation operation = new CRUDOperation();
+            if (item == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                var dbitem = operation.UpdateUser(item);
                 return Ok(dbitem);
-            }
-
-            [HttpPost]
-            [ResponseType(typeof(tbl_User))]
-            [Route("UpdateUser")]
-            public async Task<IHttpActionResult> UpdateUser(tbl_User item)
-            {
-                CRUDOperation operation = new CRUDOperation();
-                if (item == null)
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    var dbitem = operation.UpdateUser(item);
-                    return Ok(dbitem);
-                }
-            }
-
-            [HttpPost]
-            [ResponseType(typeof(tbl_User))]
-            [Route("DeleteUser/{id}")]
-            public async Task<IHttpActionResult> DeleteUser(Int64 id)
-            {
-                CRUDOperation operation = new CRUDOperation();
-
-                var dbitem = operation.DeleteUser(id, 0);
-                return Ok(dbitem);
-
-            }
-
-            [HttpPost]
-            [ResponseType(typeof(tbl_User))]
-            [Route("ChangePassword/{id}/{newpassword}")]
-            public async Task<IHttpActionResult> ChangePassword(Int64 id,string newpassword)
-            {
-                CRUDOperation operation = new CRUDOperation();
-
-                var dbitem = operation.ChangePassword(id, 0,newpassword);
-                return Ok(dbitem);
-
             }
         }
+
+        [HttpPost]
+        [ResponseType(typeof(tbl_User))]
+        [Route("DeleteUser/{id}")]
+        public async Task<IHttpActionResult> DeleteUser(Int64 id)
+        {
+            CRUDOperation operation = new CRUDOperation();
+
+            var dbitem = operation.DeleteUser(id, 0);
+            return Ok(dbitem);
+
+        }
+
+        [HttpPost]
+        [ResponseType(typeof(tbl_User))]
+        [Route("ChangePassword/{id}/{newpassword}")]
+        public async Task<IHttpActionResult> ChangePassword(Int64 id, string newpassword)
+        {
+            CRUDOperation operation = new CRUDOperation();
+
+            var dbitem = operation.ChangePassword(id, 0, newpassword);
+            return Ok(dbitem);
+
+        }
     }
+
 }
+
